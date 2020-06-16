8911bbaddb51c2a844f97c2a9bba8315eac98f51
jmales <jumales@gmail.com>
2020-06-16 17:30:12 +0200

    alert dailog

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import Book from "./book";
 import BookAuthors from "./bookAuthors";
+import AlertDialog from "./alertDialog";
 
 class Books extends Component {
   state = {
+    showDeleteDialog: false,
+    selectedBook: {},
     books: [
       {
         id: 1,
@@ -25,42 +28,66 @@ class Books extends Component {
 
   onEdit = (book) => {
     console.log("OnEditClicked", book);
+    //let bookCopy = (...this.state.books);
   };
 
   onDelete = (book) => {
     console.log("OnDelete", book);
+    this.setState({ selectedBook: book, showDeleteDialog: true });
   };
 
   onRemoveAuthor = (author) => {
     console.log("onRemoveAuthor", author);
   };
 
+  onAddAuthor = (book) => {};
+
+  deleteBook = (book) => {
+    const books = this.state.books.filter((b) => b.id !== book.id);
+    this.setState({ books, showDeleteDialog: false });
+  };
+
+  cancelDeleteDialog = () => {
+    this.setState({ showDeleteDialog: false });
+  };
+
   render() {
     return (
-      <table className="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">IBN</th>
-            <th scope="col">Title</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.books.map((book) => (
-            <React.Fragment>
-              <Book
-                key={book.id}
-                book={book}
-                onEdit={this.onEdit}
-                onDelete={this.onDelete}
-                onRemoveAuthor={this.onRemoveAuthor}
-              />
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
+      <React.Fragment>
+        <AlertDialog
+          show={this.state.showDeleteDialog}
+          handleDelete={this.deleteBook}
+          handleClose={this.cancelDeleteDialog}
+          item={this.state.selectedBook}
+        />
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">IBN</th>
+              <th scope="col">Title</th>
+              <th scope="col">Authors</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.books.map((book) => (
+              <React.Fragment>
+                <Book
+                  key={book.id}
+                  book={book}
+                  onEdit={this.onEdit}
+                  onDelete={this.onDelete}
+                  onRemoveAuthor={this.onRemoveAuthor}
+                  onAddAuthor={this.onAddAuthor}
+                />
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
